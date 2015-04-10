94e9cb19ed417a5b45a46175e66f50a1dbf1b7c7
a.l.e <ale@ideale.ch>
2015-04-10 18:50:26 +0200

    apply pippin's patch for automatically expanding slots referenced from the #part of the url and show the anchor in the url when expanding

--- wp/wp-content/themes/lgm15/js/2015_schedule.js
+++ wp/wp-content/themes/lgm15/js/2015_schedule.js
@@ -3,17 +3,48 @@
  */
 
 jQuery(document).ready(function ($) {
-	$(".schedule_summary").before("<p class=\"schedule_toggle\"><a href=\"\">+ details</a></p>");
-	$(".schedule_summary").hide();
-	$('.schedule_toggle a').click(function(e){
-		e.preventDefault();
-		if ($(this).parent().next().is(":visible")) {
-		    $(this).text("+ details");
-		} else {
-		    $(this).html("&ndash; details");
-		}
-		$(this).parent().next().toggle();
 
-		e.preventDefault();
+  /*
+   * - show a "+/- details" for expand the slots.
+   * - update the url#fragment when expanding details sections. (pippin)
+   * - open and scroll to the specified one when opening the url contains a ##fragment. (pippin)
+   */
+
+  var fragment = document.location.hash
+  var target = undefined;
+
+  if (fragment)
+    fragment = fragment.substring(1)
+
+	$(".schedule_summary").each(function (){
+    var foo = $(this).prev().prev().children(":first")[0].name;
+
+    if (foo == fragment)
+    {
+      $(this).before("<p class=\"schedule_toggle\"><a href=\"#" + foo + "\">- details </a></p>");
+      $(this).show();
+      target = $(this).parent()[0];
+    }
+    else
+    {
+      $(this).before("<p class=\"schedule_toggle\"><a href=\"#" + foo + "\">+ details </a></p>");
+      $(this).hide();
+    }
+  })
+
+	$('.schedule_toggle a').click(function(e){
+      e.preventDefault();
+      if ($(this).parent().next().is(":visible")) {
+          $(this).text("+ details");
+      } else {
+          $(this).html("&ndash; details");
+      }
+		  $(this).parent().next().toggle();
+      document.location.hash = "##" + ($(this)[0].href).replace(/.*#/, '');
+		  e.preventDefault();
     });
+  if (target)
+  {
+    setTimeout(function(){target.scrollIntoView();}, 500)
+  }
 })
